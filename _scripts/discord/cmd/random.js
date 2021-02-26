62f4874cac220a48c7f256b3dfb82caf7ba3b0a3
fr1t2 <fr1t2@protonmail.com>
2021-02-25 17:22:58 -0700

    final response

--- _scripts/discord/cmd/random.js
+++ _scripts/discord/cmd/random.js
@@ -68,7 +68,7 @@ module.exports = {
         .setColor('BLUE')
         .setTitle('League Of Entropy Information')
         .setURL('https://www.cloudflare.com/leagueofentropy/')
-        .setDescription('We are big fans or genuine random data at the QRL, and it looks like you are as well!\nThe QRL submits entropy from our blockchain to the DRAND network, helping make the random data more random. We thought this was cool and we wanted to share.')
+        .setDescription('The QRL submits entropy from our blockchain to the DRAND network, helping make the random data more random. We thought this was really cool and wanted to share!')
         .setThumbnail('https://github.com/drand/website/raw/master/docs/.vuepress/public/images/logo-drand-text-right-dark.png')
         .addFields(
           { name: 'About LoE', value: 'The League of Entropy is tackling one of the more difficult problems with cryptography, *randomness*. To learn more have a look at the links below to see how this technology hopes to make the world a little more random.', inline: false },
