763058448774dc692f9f65d332351de19a883c6b
fr1t2 <fr1t2@protonmail.com>
2021-03-17 15:34:06 -0600

    testing fix

--- _scripts/discord/cmd/tip.js
+++ _scripts/discord/cmd/tip.js
@@ -37,7 +37,7 @@ module.exports = {
       // ^\u\]/.test()
       let test = false;
       // eslint-disable-next-line
-      if(/[\u0000-\u00FF][^a-zA-Z0-9]/.test(userName)) {
+      if(/[^\u0000-\u00FF][^a-zA-Z0-9]/.test(userName)) {
         test = true;
       }
       return test;
